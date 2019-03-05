2c9efb0336be7be2c20160ffc0d6bd472c9430b6
paulinedev <pauline.de.vallois@gmail.com>
2019-03-05 13:38:52 +0100

    Météo
    ajout météo !

--- js/londres.js
+++ js/londres.js
@@ -11,4 +11,10 @@ $( document ).ready(function() {
       $(this).css('opacity','1');
       $('#londres2').css('opacity', '0');
     });
+    $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=2ebe19fb2e7dc8831190daf8260d7761", function (response){
+        var temperature = response.main.temp;
+        // alert("Il fait actuellement "+temperature+"°C à Londres !");
+        var changeTexteTemp = document.getElementById("temperature");
+        changeTexteTemp.innerHTML = "Il fait actuellement "+temperature+"°C à Londres !";
+    });
 });
