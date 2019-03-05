a336f50c902c212549942ebb524fbb17a37e4069
paulinedev <pauline.de.vallois@gmail.com>
2019-03-05 14:10:26 +0100

    meteo#2
    lol

--- js/londres.js
+++ js/londres.js
@@ -11,10 +11,18 @@ $( document ).ready(function() {
       $(this).css('opacity','1');
       $('#londres2').css('opacity', '0');
     });
+    //LA METEO
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=2ebe19fb2e7dc8831190daf8260d7761", function (response){
         var temperature = response.main.temp;
+        var rain = response.rain.1h;
+        var cloud = response.clouds.all;
+
+        // afficher soleil etc.
+        var changeImage = document.getElementsByClassName('illu');
+        if( rain)
         // alert("Il fait actuellement "+temperature+"°C à Londres !");
         var changeTexteTemp = document.getElementById("temperature");
         changeTexteTemp.innerHTML = "Il fait actuellement "+temperature+"°C à Londres !";
     });
+    
 });
--- js/rome.js
+++ js/rome.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
 	Sticker.init('.sticker');
+	//LA METEO
+	$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Roma&units=metric&appid=2ebe19fb2e7dc8831190daf8260d7761", function (response){
+			var temperature = response.main.temp;
+			// alert("Il fait actuellement "+temperature+"°C à Rome !");
+			var changeTexteTemp = document.getElementById("temperature");
+			changeTexteTemp.innerHTML = "Il fait actuellement "+temperature+"°C à Rome !";
+	});
 });
