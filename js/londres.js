bf085f181f95a5369afef96f35613560366dbcaa
LauraDuf <34089265+LauraDuf@users.noreply.github.com>
2019-07-06 21:55:49 +0200

    Météo
    Météo Maj & fonctionne :D

--- js/londres.js
+++ js/londres.js
@@ -15,25 +15,26 @@ $( document ).ready(function() {
     //LA METEO
     $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=london,uk&units=metric&appid=2ebe19fb2e7dc8831190daf8260d7761", function (response){
         var temperature = response.main.temp;
-        var temps = response.weather.id;
-        var cloud = response.clouds.all;
-
-        // afficher soleil etc.
-        // var changeImage = document.getElementsByClassName('illu');
-        if(temps = "Rain"){
-            $("#pluie").show();
-        } else if(temps ="Snow"){
-          $("#neige").show();
-        } else if (cloud < 50){
-            $('#ensolleille').show();
-        } else if (cloud >= 50){
-            if (cloud < 100){
-                $('#soleil').show();
-            } else {
-                $("#nuage").show();
-            }
-        }
-
+        var temps = response.weather[0].main;
+    		// var cloud = response.clouds.all;
+    		console.log(response.weather[0].main);
+    		// afficher soleil etc.
+    		var changeImage = document.getElementsByClassName('illu');
+    		if(temps == "Rain"){
+    			console.log("pluie");
+    				$("#pluie").show();
+    		}	else if (temps == "Clear"){
+    			console.log("ensolleille");
+    			$('#ensolleille').show();
+    		} else if(temps == "Snow"){
+    			console.log("neige");
+    			$("#neige").show();
+    		}  else if (temps == "Clouds"){
+    						$('#soleil').show();
+    				} else {
+    					console.log("nuage");
+    						$("#nuage").show();
+    				}
 
         // alert("Il fait actuellement "+temperature+"°C à Londres !");
         var changeTexteTemp = document.getElementById("temperature");
--- js/rome.js
+++ js/rome.js
@@ -3,24 +3,27 @@ $( document ).ready(function() {
 	//LA METEO
 	$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Roma&units=metric&appid=2ebe19fb2e7dc8831190daf8260d7761", function (response){
 		var temperature = response.main.temp;
-		var temps = response.weather.id;
-		var cloud = response.clouds.all;
-
+		var temps = response.weather[0].main;
+		// var cloud = response.clouds.all;
+		console.log(response.weather[0].main);
 		// afficher soleil etc.
-		// var changeImage = document.getElementsByClassName('illu');
-		if(temps = "Rain"){
+		var changeImage = document.getElementsByClassName('illu');
+		if(temps == "Rain"){
+			console.log("pluie");
 				$("#pluie").show();
-		} else if(temps ="Snow"){
+		}	else if (temps == "Clear"){
+			console.log("ensolleille");
+			$('#ensolleille').show();
+		} else if(temps == "Snow"){
+			console.log("neige");
 			$("#neige").show();
-		} else if (cloud < 50){
-				$('#ensolleille').show();
-		} else if (cloud >= 50){
-				if (cloud < 100){
+		}  else if (temps == "Clouds"){
 						$('#soleil').show();
 				} else {
+					console.log("nuage");
 						$("#nuage").show();
 				}
-		}			// alert("Il fait actuellement "+temperature+"°C à Rome !");
+		// alert("Il fait actuellement "+temperature+"°C à Rome !");
 			var changeTexteTemp = document.getElementById("temperature");
 			changeTexteTemp.innerHTML = "Il fait actuellement "+temperature+"°C à Rome !";
 	});
